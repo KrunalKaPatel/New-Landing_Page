4098c031dd776046f3e22151fc94c308be558f16
KrunalKaPatel <krunal9611@gmail.com>
2020-04-13 19:42:38 +0530

    New Section Added

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
 	$('.recent-project-slider').slick({
-  		infinite: true,
-  		slidesToShow: 1,
-  		slidesToScroll: 3,
-  		responsive: [
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        infinite: true,
-		        slidesToShow: 1,
-		        slidesToScroll: 1,
-		      }
-		    }
-    	],
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 480,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				}
+			}
+		]
 	});
 });
