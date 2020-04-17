b4b29afeaf0d89b9135741a8470335cf365f1a37
KrunalKaPatel <krunal9611@gmail.com>
2020-04-17 14:39:01 +0530

    Apna message

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+
+	/* Recent Sroject Slider
+	------------------------------*/
 	$('.recent-project-slider').slick({
 		infinite: true,
-		arrows: false,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		responsive: [
@@ -33,9 +35,32 @@ $(document).ready(function(){
 
 	$('.testimonial-slider').slick({
   		infinite: true,
+		arrows: false,
   		slidesToShow: 1,
   		slidesToScroll: 1
 	});
 
-
+	/* Our Patners Slider
+	------------------------------*/
+	$('.our-patners-slider').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				}
+			}
+		]
+	});
 });
